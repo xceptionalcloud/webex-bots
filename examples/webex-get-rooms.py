5d31b005006f32e9bf7e57766f66d1d3f1e1ec39
xndev <55002728+xndev@users.noreply.github.com>
2020-03-04 14:56:47 -0800

    Add files via upload

--- examples/webex-get-rooms.py
+++ examples/webex-get-rooms.py
@@ -2,7 +2,7 @@ import configparser
 import requests
 config = configparser.RawConfigParser()
 config.read('settings.ini')
-auth = config['GIPHIER']['Token']
+auth = config['KHALKEUS']['Token']
 
 r = requests.get('https://api.ciscospark.com/v1/rooms',
                   headers={'Authorization': 'Bearer {auth}'.format(auth=auth)})
