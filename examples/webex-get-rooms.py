4fde79e72415ffa77a58a1e0fc80365a17be7b12
xndev <alangford@xceptional.com>
2020-03-04 15:07:13 -0800

    changes

--- examples/webex-get-rooms.py
+++ examples/webex-get-rooms.py
@@ -1,8 +1,9 @@
 import configparser
 import requests
 config = configparser.RawConfigParser()
-config.read('settings.ini')
-auth = config['KHALKEUS']['Token']
+config.read('var/www/settings.ini')
+#auth = config['KHALKEUS']['Token']
+auth = config['AARON']['Token']
 
 r = requests.get('https://api.ciscospark.com/v1/rooms',
                   headers={'Authorization': 'Bearer {auth}'.format(auth=auth)})
